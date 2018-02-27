201b5d96a4643362320c048070787cf0c4b4b285
Georg Drees <tuxlifan@derseegrog.org>
2018-02-27 17:39:19 +0100

    Revert "Activate and hide the window when clicking on the tray icon when using i3wm"
    This reverts commit e560693c7736272d18029c368bd10951e0329e86. To have clean PR for search modification.

--- client/signetapplication.cpp
+++ client/signetapplication.cpp
@@ -234,7 +234,6 @@ void SignetApplication::trayActivated(QSystemTrayIcon::ActivationReason reason)
 				m_main_window->setWindowState((m_main_window->windowState() & ~Qt::WindowMinimized) | Qt::WindowActive);
 			}
 			m_main_window->raise();
-			m_main_window->activateWindow();
 		} else {
 			if (m_main_window->isMinimized()) {
 				m_main_window->setWindowState((m_main_window->windowState() & ~Qt::WindowMinimized) | Qt::WindowActive);
@@ -242,7 +241,6 @@ void SignetApplication::trayActivated(QSystemTrayIcon::ActivationReason reason)
 			} else {
 				m_main_window->setWindowState((m_main_window->windowState() & ~Qt::WindowActive) | Qt::WindowMinimized);
 			}
-                        m_main_window->hide();
 		}
 		break;
 	default:
