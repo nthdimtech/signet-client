bb8148a37e88adec966ca20497b2f348d6505475
Georg Drees <tuxlifan@derseegrog.org>
2018-03-02 15:16:38 +0100

    Add key accelerators for main functionalities in Settings

--- client/settingsdialog.cpp
+++ client/settingsdialog.cpp
@@ -29,7 +29,7 @@ SettingsDialog::SettingsDialog(MainWindow *mainWindow, bool initial) :
 	m_activeKeyboardLayout = m_settings->activeKeyboardLayout;
 	m_keyboardLayouts = m_settings->keyboardLayouts;
 
-	m_localBackups = new QCheckBox("Enable local backups");
+	m_localBackups = new QCheckBox("Enable &local backups");
 	m_localBackups->setChecked(m_settings->localBackups);
 
 	m_localBackupPath = new QLineEdit(m_settings->localBackupPath);
@@ -41,7 +41,7 @@ SettingsDialog::SettingsDialog(MainWindow *mainWindow, bool initial) :
 	m_localBackupInterval->setMaximum(60);
 	m_localBackupInterval->setValue(m_settings->localBackupInterval);
 
-	m_removableBackups = new QCheckBox("Enable removable backups");
+	m_removableBackups = new QCheckBox("Enable &removable backups");
 	m_removableBackups->setChecked(m_settings->removableBackups);
 
 	m_removableBackupVolume = new QLineEdit(m_settings->removableBackupVolume);
@@ -86,10 +86,10 @@ SettingsDialog::SettingsDialog(MainWindow *mainWindow, bool initial) :
 	QHBoxLayout *keyboardLayoutConfigurationLayout = new QHBoxLayout();
 	if (keyboardLayoutConfigured) {
 		keyboardLayoutConfigurationLayout->addWidget(new QLabel("Keyboard layout"));
-		m_configureKeyboardLayout = new QPushButton("Reconfigure");
+		m_configureKeyboardLayout = new QPushButton("Re&configure");
 	} else {
 		keyboardLayoutConfigurationLayout->addWidget(new QLabel("Keyboard layout"));
-		m_configureKeyboardLayout = new QPushButton("Configure");
+		m_configureKeyboardLayout = new QPushButton("&Configure");
 	}
 	keyboardLayoutConfigurationLayout->addWidget(m_configureKeyboardLayout);
 	connect(m_configureKeyboardLayout, SIGNAL(pressed()),
