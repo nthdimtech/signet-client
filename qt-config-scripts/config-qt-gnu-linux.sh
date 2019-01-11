6e89f9912680fee2e8a6582804d33878ed37ee55
Neils Nesse <nnesse@nthdimtech.com>
2019-01-11 13:12:48 -0800

    Include scripts for configuring static Qt libraries for Signet
    Starting with GNU/Linux and MSYS2 (Windows) for now. Presently MacOS
    static binaries linked with libraries provided by brew. This will likely
    change in the future.

---
+++ qt-config-scripts/config-qt-gnu-linux.sh
@@ -0,0 +1,26 @@
+#!/bin.bash
+$1/configure \
+-platform linux-g++ -static -opengl desktop \
+-opensource -confirm-license \
+-prefix $2 \
+-skip qt3d \
+-skip qtcanvas3d \
+-skip qtdatavis3d \
+-skip qtgamepad \
+-skip qtactiveqt \
+-skip qtcharts \
+-skip qtgraphicaleffects \
+-skip qtimageformats \
+-skip qtpurchasing \
+-skip qttools \
+-skip qtwayland \
+-skip qtwebchannel \
+-skip qtwebengine \
+-skip qtwebsockets \
+-skip qtwebview \
+-skip qtlocation \
+-skip qtfeedback \
+-skip qtdeclarative \
+-nomake examples -nomake tests \
+-no-icu -no-mtdev -no-eglfs -no-linuxfb -no-libudev -no-egl -no-gstreamer -no-gbm -no-xcb-native-painting \
+-qt-libjpeg -qt-libpng -qt-xcb -qt-pcre
---
+++ qt-config-scripts/config-qt-msys2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+$1/configure \
+-platform win32-g++ -static -release -opengl desktop \
+-opensource -confirm-license \
+-prefix $2 \
+-skip qttools \
+-skip qt3d \
+-skip qtactiveqt \
+-skip qtcanvas3d \
+-skip qtcharts \
+-skip qtconnectivity \
+-skip qtdatavis3d \
+-skip qtdeclarative \
+-skip qtdoc \
+-skip qtgamepad \
+-skip qtgraphicaleffects \
+-skip qtimageformats \
+-skip qtlocation \
+-skip qtpurchasing \
+-skip qtremoteobjects \
+-skip qtsensors \
+-skip qtsvg \
+-skip qttranslations \
+-skip qtwebchannel \
+-skip qtwebengine \
+-skip qtwebglplugin \
+-skip qtwebsockets \
+-skip qtwebview \
+-nomake examples -nomake tests \
+-no-iconv -no-gstreamer -no-glib -no-sql-ibase -no-sql-mysql -no-sql-odbc -no-sql-psql -no-sql-sqlite2 -no-dbus -no-fontconfig \
+-sql-sqlite -qt-sqlite \
+-qt-freetype -qt-harfbuzz -qt-libjpeg -qt-zlib -qt-pcre -qt-libpng
